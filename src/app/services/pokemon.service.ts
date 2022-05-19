aa43386145cfac2be5f613ae20608280839b00b6
Luiz Felipe Santana Dos Santos <luiz.santos811@etec.sp.gov.br>
2022-05-19 11:01:29 +0000

    AppPokedex

---
+++ src/app/services/pokemon.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokemonService {
+
+  private url='https://pokeapi.co/api/v2/';
+
+  constructor() {
+   }
+}
